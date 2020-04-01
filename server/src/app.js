63880b43664042f832170b6ca7cd964b2c2bddfb
Emma <enabojohn@gmail.com>
2020-04-01 13:43:48 +0300

    Configured eslint to run over the server side

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,26 @@
-console.log("Heloo world.....");
+const express = require('express');
+const bodyParser = require('body-parser');//Process json data very easily
+const cors = require('cors');
+const morgan = require('morgan');
+//Innitialising the app
+const app = express();
 
+
+app.use(morgan('combine'));
+app.use(bodyParser.json());
+app.use(cors()); //Enables any client around the world to hit the server(security risk to have it enabled)
+
+app.get('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}! Your user was registered have fun`
+    });
+});
+
+
+// app.listen(process.env.PORT || 8081);
+app.listen(3000, () => {
+    console.log(``);
+});
 
 
 
